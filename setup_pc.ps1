e25b4d791c32eb659856991ca6aaf89b95eece32
ElmarJ <elmar@elmarjansen.nl>
2018-05-13 16:06:18 +0200

    Initial commit

---
+++ setup_pc.ps1
@@ -0,0 +1,51 @@
+# Description: Boxstarter Script
+# Author: Elmar
+# All my favorite apps
+
+#--- Basics ---
+choco install googledrive
+choco install steam
+choco install spotify 
+# choco install f.lux 
+
+#--- Tools ---
+choco install -y vscode
+choco install -y git -params '"/GitAndUnixToolsOnPath /WindowsTerminal"'
+choco install -y 7zip.install
+
+#--- Statistics ---
+choco install anaconda3 
+choco install r.project
+choco install r.studio 
+
+#--- Music ---
+choco install audacity 
+
+#--- Windows Subsystems/Features ---
+choco install -y Microsoft-Hyper-V-All -source windowsFeatures
+choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+
+#--- Ubuntu ---
+# Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile ~/Ubuntu.appx -UseBasicParsing
+# Add-AppxPackage -Path ~/Ubuntu.appx
+
+#--- Browsers ---
+choco install -y Firefox
+choco install -y Googlechrome
+
+#--- Fonts ---
+choco install -y inconsolata
+# choco install -y ubuntu.font
+
+#--- Tools ---
+choco install -y nodejs-lts # Node.js LTS, Recommended for most users
+# choco install -y nodejs # Node.js Current, Latest features
+choco install -y visualstudio2017buildtools
+choco install -y visualstudio2017-workload-vctools
+choco install -y sysinternals
+choco install -y docker-for-windows
+choco install -y python2 # Node.js requires Python 2 to build native modules
+
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
